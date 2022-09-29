715438896455099da78504ad7386c9515aee9f73
krJean <kaliregenold@gmail.com>
2022-09-29 17:43:54 -0400

    Add randomization and scoring.

--- img_approx.py
+++ img_approx.py
@@ -1,12 +1,93 @@
+from signal import SIG_DFL
 from shapes import Circle
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageChops, ImageStat
+import copy
+import signal
+import sys
+from random import randint
+import cv2
+import numpy as np
+
+def generate_shapes(num_shapes:int, canvas_size:tuple[int]) -> list[Circle]:
+    shape_list = [None]*num_shapes
+    for i in range(num_shapes):
+        shape_list[i] = Circle(max_radius=min(canvas_size)//20, max_center=canvas_size)
+        shape_list[i].randomize()
+    return shape_list
+
+def get_score(img1, img2):
+    diff = ImageChops.difference(img1, img2)
+    stat = ImageStat.Stat(diff)
+    return stat.mean
+
+def is_better(old_score, new_score):
+    r = old_score[0]-new_score[0]
+    g = old_score[1]-new_score[1]
+    b = old_score[2]-new_score[2]
+    return (r+b+g) > 0
+
+def draw_shapes(draw_context, shape_list):
+    for s in shape_list:
+        draw_context.ellipse(xy=s.get_bbox(), fill=tuple(s.color))
+
+def wiggle_shapes(shape_list):
+    for s in shape_list:
+        s.wiggle()
+
+def clear_canvas(draw_context, canvas_size):
+    draw_context.rectangle([(0,0),canvas_size],fill=(0,0,0))
 
 def main():
-    my_shape = Circle(color=[46, 176, 100], center=[50,50], radius=10)
-    canvas = Image.new(mode='RGB', size=(100,100), color=(255,255,255))
+    # Initialize the canvases
+    target = Image.open(fp='starry-night-big.jpg').convert(mode='RGB')
+    canvas_size = target.size
+    canvas = Image.new(mode='RGB', size=canvas_size, color=(0,0,0))
     draw = ImageDraw.Draw(canvas)
-    draw.ellipse(xy=my_shape.get_bbox(), fill=tuple(my_shape.color))
+
+    # Create initial list of shapes
+    shape_list = generate_shapes(300, canvas_size)
+
+    # Track number of improvements
+    improvement_count = 0
+
+    # First round of randomization
+    wiggle_shapes(shape_list)
+    draw_shapes(draw,shape_list)
+
+    # Keep score
+    old_score = get_score(canvas, target)
+    old_shapes = copy.deepcopy(shape_list)
+    print("Old score: ", old_score)
+    new_score = copy.copy(old_score)
+
+    # Handle an early exit
+    def signal_handler(sig, frame):
+        clear_canvas(draw, canvas_size)
+        draw_shapes(draw, old_shapes)
+        canvas.show()
+        target.show()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Do the thing
+    while improvement_count < 10000:
+        while(not is_better(old_score, new_score)):
+            clear_canvas(draw, canvas_size)
+            new_shapes = copy.deepcopy(old_shapes)
+            new_shapes[randint(0,len(new_shapes)-1)].wiggle()
+            new_shapes.sort(key=lambda x: x.get_area(), reverse=True)
+            draw_shapes(draw, new_shapes)
+            new_score = get_score(canvas, target)
+        if improvement_count % 10 == 0:
+            print(str(improvement_count)+": "+str(new_score))
+            cv2.imshow('approx', cv2.cvtColor(np.array(canvas).copy(), cv2.COLOR_RGB2BGR))
+            cv2.waitKey(1)
+        improvement_count += 1
+        old_score = copy.copy(new_score)
+        old_shapes = copy.deepcopy(new_shapes)
+
     canvas.show()
+    target.show()
 
 if __name__ == '__main__':
     main()
--- shapes.py
+++ shapes.py
@@ -1,18 +1,30 @@
 from abc import ABC
-
+from random import randint
 
 class Shape(ABC):
-    def __init__(self, color:list[int]=[0,0,0]):
+    def __init__(self, color:tuple[int]=[0,0,0]):
         self.color = color
 
     def get_color(self) ->tuple[int]:
         return tuple(self.color)
 
 class Circle(Shape):
-    def __init__(self, color:list[int]=[0,0,0], center:list[int]=[0,0], radius:int=0):
+    def __init__(self, color:tuple[int]=(0,0,0),
+                       center:list[int]=(0,0),
+                       radius:int=0,
+                       max_radius:int=10,
+                       max_center:list[int]=(10,10)):
         super().__init__(color)
         self.center = center
         self.radius = radius
+        self.max_center = max_center
+        self.max_radius = max_radius
+
+    def _wiggle_val(self, val, min_val, max_val, wiggle_val):
+        val += randint(-wiggle_val,wiggle_val)
+        val = max(val,min_val)
+        val = min(val,max_val)
+        return val
 
     def get_bbox(self) -> list[tuple[int]]:
         x0 = self.center[0] - self.radius
@@ -20,3 +32,23 @@ class Circle(Shape):
         y0 = self.center[1] - self.radius
         y1 = self.center[1] + self.radius
         return [(x0,y0),(x1,y1)]
+
+    def randomize(self):
+        self.color = tuple(randint(0,255) for _ in range(3))
+        self.center = tuple(randint(0,self.max_center[i]) for i in range(2))
+        self.radius = randint(0,self.max_radius)
+
+    def wiggle(self):
+        rand_trait = randint(0,2)
+        #color
+        if rand_trait == 0:
+            self.color = tuple(self._wiggle_val(self.color[i], 0, 255, 10) for i in range(3))
+        #center
+        elif rand_trait == 1:
+            self.center = tuple(self._wiggle_val(self.center[i], 0, self.max_center[i], 40) for i in range(2))
+        #radius
+        elif rand_trait == 2:
+            self.radius = self._wiggle_val(self.radius, 4, self.max_radius, 40)
+
+    def get_area(self):
+        return 3.14*self.radius*self.radius
